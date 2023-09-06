b50479f3b4b0d4381339d774d49e566a08ceba13
Manuel Egger <manuel.egger85@gmail.com>
2023-09-06 14:46:44 +0200

    added Service Layer

--- src/main/java/com/example/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/example/cruddemo/rest/EmployeeRestController.java
@@ -2,6 +2,7 @@ package com.example.cruddemo.rest;
 
 import com.example.cruddemo.dao.EmployeeDAO;
 import com.example.cruddemo.entity.Employee;
+import com.example.cruddemo.service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,16 +13,16 @@ import java.util.List;
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-  private EmployeeDAO employeeDAO;
+  private EmployeeService employeeService;
 
   // quick solution: inject EmployeeDAO (constructor injection)
-  public EmployeeRestController(EmployeeDAO employeeDAO) {
-    this.employeeDAO = employeeDAO;
+  public EmployeeRestController(EmployeeService employeeService) {
+    this.employeeService = employeeService;
   }
 
   // expose "/employees" and return a list of employees
   @GetMapping("/employees")
   public List<Employee> findAll() {
-    return employeeDAO.findAll();
+    return employeeService.findAll();
   }
 }
