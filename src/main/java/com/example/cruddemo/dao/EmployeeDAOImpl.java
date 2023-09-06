1e7eea4e19da31c8b9f6e98ebba93a6b2633cfbb
Manuel Egger <manuel.egger85@gmail.com>
2023-09-06 14:29:48 +0200

    added EmployeeDAO and implemented EmployeeDAOImpl

---
+++ src/main/java/com/example/cruddemo/dao/EmployeeDAOImpl.java
@@ -0,0 +1,34 @@
+package com.example.cruddemo.dao;
+
+import com.example.cruddemo.entity.Employee;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+  // define field for EntityManager
+  private EntityManager entityManager;
+
+  // set up constructor injection
+  @Autowired
+  public EmployeeDAOImpl(EntityManager entityManager) {
+    this.entityManager = entityManager;
+  }
+
+  @Override
+  public List<Employee> findAll() {
+    // create a query
+    TypedQuery<Employee> myQuery = entityManager.createQuery("FROM Employee", Employee.class);
+
+    // execute query and get result list
+    List<Employee> employees = myQuery.getResultList();
+
+    // return the results
+    return employees;
+  }
+}
